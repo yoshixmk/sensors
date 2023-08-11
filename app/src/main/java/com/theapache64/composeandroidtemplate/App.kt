49b9817af88796fe3da4491b9e6bc8535813f3a7
yoshixmk <pirori67@gmail.com>
2023-08-11 19:06:41 +0900

    rename packages

--- app/src/main/java/com/theapache64/composeandroidtemplate/App.kt
+++
@@ -1,7 +0,0 @@
-package com.theapache64.composeandroidtemplate
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()
---
+++ app/src/main/java/com/yoshixmk/sensors/App.kt
@@ -0,0 +1,7 @@
+package com.yoshixmk.sensors
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
