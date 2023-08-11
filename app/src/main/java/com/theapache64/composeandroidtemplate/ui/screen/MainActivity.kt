132d5d67ef5fee77b3f311ee87bd62d2e6e0d16b
Yoshihiro Ueki <pirori67@gmail.com>
2023-08-11 16:10:24 +0900

    Initial commit

---
+++ app/src/main/java/com/theapache64/composeandroidtemplate/ui/screen/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.theapache64.composeandroidtemplate.ui.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.theapache64.composeandroidtemplate.ui.screen.dashboard.DashboardScreen
+import com.theapache64.composeandroidtemplate.ui.screen.splash.SplashScreen
+import com.theapache64.composeandroidtemplate.ui.theme.ComposeAndroidTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAndroidTemplateTheme {
+                Surface {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun AppNavigation() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Screen.Splash.route) {
+
+            // Splash
+            composable(Screen.Splash.route) {
+                SplashScreen(
+                    onSplashFinished = {
+                        val options = NavOptions.Builder()
+                            .setPopUpTo(Screen.Splash.route, inclusive = true)
+                            .build()
+                        navController.navigate(
+                            Screen.Dashboard.route,
+                            options
+                        ) // Move to dashboard
+                    }
+                )
+            }
+
+            // Dashboard
+            composable(Screen.Dashboard.route) {
+                DashboardScreen()
+            }
+        }
+    }
+}
